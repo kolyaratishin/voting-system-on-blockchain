b762e7af34bd5a252cba26689dfbb3c6b48fe362
mykola.ratyshyn <kolyaratishin1@gmail.com>
2024-10-28 17:39:54 +0200

    project init

---
+++ src/main/java/com/voting_system_on_blockchain/VotingSystemOnBlockchainApplication.java
@@ -0,0 +1,13 @@
+package com.voting_system_on_blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemOnBlockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSystemOnBlockchainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voting_system_on_blockchain/VotingSystemOnBlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voting_system_on_blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemOnBlockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
